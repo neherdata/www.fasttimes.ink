ea131a15fdf251284b6e08ec1bd666d074c833d3
Tyler M. Neher <tyler@neherdata.com>
2023-02-23 00:41:51 -0500

    reorg
    Signed-off-by: Tyler M. Neher <tyler@neherdata.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Artists from "./Artists";
-import ContactUs from "./Contact";
-import Faq from "./Faq";
-import Navi from "./Nav";
+import Home from "./Components/Home";
+import Artists from "./Components/Artists";
+import ContactUs from "./Components/Contact";
+import Faq from "./Components/Faq";
+import Navi from "./Components/Nav";
 
 const App = () => {
   return (
