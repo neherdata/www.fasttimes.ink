a33f3788b2a9529e8565cb1434f5c37eea59b46b
Tyler M. Neher <tyler@neherdata.com>
2023-02-23 01:56:01 -0500

    maye?
    Signed-off-by: Tyler M. Neher <tyler@neherdata.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "App";
+import App from "./App";
 import { HashRouter } from "react-router-dom";
+require("@babel/core").transformSync("code", {
+  plugins: ["@babel/plugin-syntax-jsx"],
+});
 
 const root = createRoot(document.querySelector("#root"));
 
