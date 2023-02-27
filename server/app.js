91c9b7ddc3947f36affb4762b69170e2a312c07e
Christian Cifuentes <ccifuent1@gmail.com>
2023-02-27 00:31:28 +0000

    server files working

--- server/app.js
+++ server/app.js
@@ -1,28 +1,28 @@
-import express, { json, static } from "express";
+const express = require("express");
 const app = express();
-import { join } from "path";
+const path = require("path");
 
-export default app;
+module.exports = app;
 
-app.use(json());
-app.use("/dist", static(join(__dirname, "../dist")));
-app.use("/static", static(join(__dirname, "../static")));
+app.use(express.json());
+app.use("/dist", express.static(path.join(__dirname, "../dist")));
+app.use("/static", express.static(path.join(__dirname, "../static")));
 
 app.get("/", (req, res) =>
-  res.sendFile(join(__dirname, "../static/index.html"))
+  res.sendFile(path.join(__dirname, "../static/index.html"))
 );
 
-app.get("/artists", (req, res) => {
-  res.sendFile(join(__dirname, "../static/index.html"));
-});
+// app.get("/artists", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../static/index.html"));
+// });
 
-app.get("/contact", (req, res) => {
-  res.sendFile(join(__dirname, "../static/index.html"));
-});
+// app.get("/contact", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../static/index.html"));
+// });
 
-app.get("/faq", (req, res) => {
-  res.sendFile(join(__dirname, "../static/index.html"));
-});
+// app.get("/faq", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../static/index.html"));
+// });
 
 app.use((err, req, res, next) => {
   res.status(500).send(err);
