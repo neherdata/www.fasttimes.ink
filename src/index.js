b4c2a5c04f04bb7af0d3389a99d1dba07d0f3fc8
Christian Cifuentes <ccifuent1@gmail.com>
2023-04-14 19:28:49 +0000

    path revert

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./src/Components/App";
+import App from "./Components/App";
 import { HashRouter } from "react-router-dom";
-import store from "./src/store";
+import store from "./store";
 import { Provider } from "react-redux";
 
 const root = createRoot(document.querySelector("#root"));
