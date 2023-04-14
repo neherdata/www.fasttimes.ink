6167e15d4f4f36ddc73c9eee5fc84cd65f979672
Christian Cifuentes <ccifuent1@gmail.com>
2023-04-14 19:22:52 +0000

    path changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./Components/App";
+import App from "./src/Components/App";
 import { HashRouter } from "react-router-dom";
-import store from "./store";
+import store from "./src/store";
 import { Provider } from "react-redux";
 
 const root = createRoot(document.querySelector("#root"));
